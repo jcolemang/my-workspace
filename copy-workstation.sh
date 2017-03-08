8d553078c7c2267b1ca14bc8d29741adc6781aad
Coleman <gibsonjc@rose-hulman.edu>
2017-03-08 14:12:18 -0500

    Added code to copy .bashrc

--- copy-workstation.sh
+++ copy-workstation.sh
@@ -2,4 +2,5 @@
 cp -r ~/.xmonad/ /home/coleman/Code/Workstation/
 cp ~/.spacemacs /home/coleman/Code/Workstation/
 cp ~/.vimrc /home/coleman/Code/Workstation/
+cp ~/.bashrc /home/coleman/Code/Workstation/
 cp ~/.emacs.d/init.el /home/coleman/Code/Workstation/.emacs.d/
