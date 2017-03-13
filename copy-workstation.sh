d8a30dad74f94e2851c5ab035fd37caab0564384
Coleman <gibsonjc@rose-hulman.edu>
2017-03-12 21:55:17 -0400

    Moved keyboard layout to xinit, added xinit

--- copy-workstation.sh
+++ copy-workstation.sh
@@ -1,8 +1,8 @@
 
-cp -r ~/.xmonad/ /home/coleman/Code/Workstation/
 cp ~/.spacemacs /home/coleman/Code/Workstation/
 cp ~/.vimrc /home/coleman/Code/Workstation/
 cp ~/.bashrc /home/coleman/Code/Workstation/
+cp ~/.xinitrc /home/coleman/Code/Workstation/
 cp ~/.emacs.d/init.el /home/coleman/Code/Workstation/.emacs.d/
+cp -r ~/.xmonad/ /home/coleman/Code/Workstation/
 
-rm -r /home/coleman/Code/Workstation/.xmonad/.stack-work/
