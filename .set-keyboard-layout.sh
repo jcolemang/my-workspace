f8e945c775dbf40170894dbad2f32715a61b36c3
Coleman <gibsonjc@rose-hulman.edu>
2017-05-16 11:10:12 -0400

    Slightly more sane keyboard

--- .set-keyboard-layout.sh
+++ .set-keyboard-layout.sh
@@ -2,9 +2,9 @@
 
 setxkbmap -layout us -variant dvp
 xset r rate 130 30
-setxkbmap -option caps:backspace
-xmodmap -e "clear Lock"
+setxkbmap -option ctrl:nocaps
+# xmodmap -e "clear Lock"
+# xmodmap -e "keycode 22 = Return"
 xmodmap -e "keycode 10 = percent ampersand"
-
-
-
+# xmodmap -e "keycode 36 = BackSpace"
+# xmodmap -e "keycode  = Control_R"
