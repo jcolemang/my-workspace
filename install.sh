d0c349e3fff0a91c718cbd9e775f297382057530
Coleman <gibsonjc@rose-hulman.edu>
2017-07-22 22:47:00 -0400

    Just a test

---
+++ install.sh
@@ -0,0 +1,11 @@
+
+sudo pacman -S vim
+sudo pacman -S conky
+sudo pacman -S stack
+sudo pacman -S ghc
+sudo pacman -S firefox
+sudo pacman -S virtualbox
+sudo pacman -S texlive-most
+sudo pacman -S rawtherapee
+sudo pacman -S git
+
