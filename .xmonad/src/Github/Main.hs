a276e5140566ebaf762b05ffeb35c70098035a3b
Coleman <gibsonjc@rose-hulman.edu>
2017-07-29 00:42:46 -0400

    Added two kinds of yaml repo urls

--- .xmonad/src/Github/Main.hs
+++ .xmonad/src/Github/Main.hs
@@ -1,47 +1,79 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 import Network.Wreq ( get
                     , responseBody )
 import System.Environment ( getArgs )
 import Data.Aeson ( FromJSON
                   , (.:)
-                  , Value ( Object )
+                  , Value ( Object
+                          , Array )
                   , parseJSON
                   , decode )
 import Control.Lens ( (^.) )
 import Control.Monad ( join
+                     , mzero
                      , guard )
 import Control.Monad.Trans ( liftIO )
 import Control.Monad.Trans.Maybe ( runMaybeT
                                  , MaybeT ( MaybeT ) )
+import Control.Applicative ( (<|>) )
 import Data.Maybe ( catMaybes )
-import Data.List ( elemIndex
-                 , isInfixOf )
 import Data.Yaml ( decodeFile )
+import Data.Vector ( toList )
 
 
 class PPrint a where
   pprint :: a -> String
 
 
-data Config =
-  Config [String]
+newtype Username = User String
+                   deriving Show
+
+newtype Repo = Repo String
+               deriving Show
+
+data Config
+  = Config [Url]
   deriving Show
 
-instance FromJSON Config where
-  parseJSON (Object o) =
-    Config <$> o .: "repos"
-  parseJSON _ =
-    mempty
-
-
-data PullRequest = PullRequest
+data PullRequest
+  = PullRequest
   { _repo        :: String
   , _title       :: String
   , _updated     :: String
   , _user        :: String
   , _description :: String
   } deriving Show
+
+data Url
+  = FullUrl String
+  | Repository Username Repo
+  deriving Show
+
+
+instance FromJSON Config where
+  parseJSON (Object o) =
+    Config
+    <$> (o .: "repos"
+         >>=
+         (\case
+             Array v -> let val = mapM parseJSON $ toList v
+                        in val
+             _ -> mzero))
+  parseJSON _ =
+    mzero
+
+instance FromJSON Url where
+  parseJSON (Object o) = do
+    repo <- o .: "repo"
+    let fullUrl   = FullUrl <$> repo .: "url"
+    let githubUrl = Repository
+                    <$> (User <$> repo .: "username")
+                    <*> (Repo <$> repo .: "repository")
+    githubUrl <|> fullUrl
+  parseJSON _ =
+    mzero
 
 instance FromJSON PullRequest where
   parseJSON (Object o) =
@@ -59,16 +91,14 @@ instance PPrint PullRequest where
     _repo pr ++ ": " ++ _title pr
 
 
-getRepoUrl :: String -> String
-getRepoUrl x =
-  if "https://" `isInfixOf`  x
-  then x
-  else case elemIndex ':' x of
-         Nothing -> x
-         Just i ->
-           let (user, repo) = splitAt i x
-           in "https://api.github.com/repos/" ++ user ++ "/" ++ drop 1 repo ++ "/pulls"
 
+getRepoUrl :: Url -> String
+getRepoUrl x =
+  case x of
+    FullUrl url ->
+      url
+    Repository (User uname) (Repo repo) ->
+      "https://api.github.com/repos/" ++ uname ++ "/" ++ repo ++ "/pulls"
 
 
 maybeGetPullRequests :: String -> MaybeT IO [PullRequest]
@@ -81,11 +111,11 @@ getAllPullRequests :: [String] -> IO [PullRequest]
 getAllPullRequests urls =
   (join . catMaybes) <$> mapM (runMaybeT . maybeGetPullRequests) urls
 
-main :: IO String
+main :: IO ()
 main = do
   args <- getArgs
   guard $ length args == 1
   mconfig <- decodeFile $ head args
   (Config urls) <- maybe (fail "Could not read config file") return mconfig
   prs <- getAllPullRequests $ getRepoUrl <$> urls
-  return . unlines $ pprint <$> prs
+  putStr . unlines $ pprint <$> prs
